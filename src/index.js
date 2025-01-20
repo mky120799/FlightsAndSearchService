d91e31c36aa261bc1979c5b8c0f1e55ea7b0d257
Mithilesh Yadav <mky1207@Mithileshs-MacBook-Air.local>
2025-01-20 11:15:33 +0530

    installed some basic dependencies and added dotenv file for environment variables

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
- 
+ setupAndstartServer= async()=>{
+
+
+    const {PORT} = require('./config/serverConfig');
+    const express = require('express');
+    const app = express();
+    const port = PORT || 3000;
+   
+    app.listen(PORT, () => {
+      console.log(`Appp is running at http://localhost:${PORT}`);
+    });
+  }  
+    setupAndstartServer();
