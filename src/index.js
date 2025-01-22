3cc7d7252134cff75aaa980f660f1a014e8dae7f
Mithilesh Yadav <mky1207@Mithileshs-MacBook-Air.local>
2025-01-22 13:44:37 +0530

    added new dependencies and working on database integration.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
- setupAndstartServer= async()=>{
+const {PORT} = require('./config/serverConfig');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+setupAndstartServer= async()=>{
 
 
-    const {PORT} = require('./config/serverConfig');
-    const express = require('express');
     const app = express();
     const port = PORT || 3000;
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true })); 
    
     app.listen(PORT, () => {
       console.log(`Appp is running at http://localhost:${PORT}`);
